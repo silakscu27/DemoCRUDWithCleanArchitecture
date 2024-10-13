04dea5aa527d4178b5d3083c0782034679a5d863
Sila Kuscu <silaksc27@gmail.com>
2024-10-13 22:19:43 +0300

    [Add] Added AppDbContext class

---
+++ DemoCRUDWithCleanArchitecture/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Domain.Entity.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
