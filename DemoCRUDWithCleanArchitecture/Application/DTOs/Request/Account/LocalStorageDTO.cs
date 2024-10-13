73b40b5ecb974e3142c67c30b7785a3e871e12b0
Sila Kuscu <silaksc27@gmail.com>
2024-10-13 21:34:13 +0300

    [Add] Added LocalStorageDTO class in request/account folder

---
+++ DemoCRUDWithCleanArchitecture/Application/DTOs/Request/Account/LocalStorageDTO.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Application.DTOs.Request.Account
+{
+    public class LocalStorageDTO
+    {
+        public string? Token { get; set; }
+        public string? Refresh { get; set;  }
+    }
+}
