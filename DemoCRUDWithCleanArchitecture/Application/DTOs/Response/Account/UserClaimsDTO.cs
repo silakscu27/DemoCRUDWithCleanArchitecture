26daa5062aa5b8fce5677d12fa649fcab5af935d
Sila Kuscu <silaksc27@gmail.com>
2024-10-13 22:02:34 +0300

    [Add] Added UserClaimsDTO class in response/account folder

---
+++ DemoCRUDWithCleanArchitecture/Application/DTOs/Response/Account/UserClaimsDTO.cs
@@ -0,0 +1,6 @@
+﻿
+namespace Application.DTOs.Response.Account
+{
+    public record UserClaimsDTO
+        (string Fullname = null!, string Username = null!, string Email = null!, string Role = null!);
+}
