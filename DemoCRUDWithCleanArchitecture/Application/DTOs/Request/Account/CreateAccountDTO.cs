a329ed9ded7eb5f1f53d1990126010fb94535025
Sila Kuscu <silaksc27@gmail.com>
2024-10-13 21:01:39 +0300

    [Update] Updated CreateAccountDTO class

--- DemoCRUDWithCleanArchitecture/Application/DTOs/Request/Account/CreateAccountDTO.cs
+++ DemoCRUDWithCleanArchitecture/Application/DTOs/Request/Account/CreateAccountDTO.cs
@@ -1,8 +1,13 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Application.DTOs.Request.Account
 {
-    internal class CreateAccountDTO
+    public class CreateAccountDTO : LoginDTO
     {
+        [Required]
+        public string Name { get; set; }
+        [Required, Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
     }
 }
