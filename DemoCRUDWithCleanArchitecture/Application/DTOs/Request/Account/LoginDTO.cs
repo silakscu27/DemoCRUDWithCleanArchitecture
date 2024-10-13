cb7130754c2a377e526d2c93967d45e4050a8ea1
Sila Kuscu <silaksc27@gmail.com>
2024-10-13 20:37:06 +0300

    [Add] Added LoginDTO class in request/account folder

---
+++ DemoCRUDWithCleanArchitecture/Application/DTOs/Request/Account/LoginDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Request.Account
+{
+    public class LoginDTO
+    {
+        [EmailAddress, Required, DataType(DataType.EmailAddress)]
+        [RegularExpression("[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+",
+            ErrorMessage = "Your mail is not valid")]
+        [Display(Name = "Email Address")]
+
+        public string EmailAddress { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
+            ErrorMessage = "Your password must be a mix of alphanumeric and special characters")]
+        public string Password { get; set; } = string.Empty;
+
+
+    }
+}
