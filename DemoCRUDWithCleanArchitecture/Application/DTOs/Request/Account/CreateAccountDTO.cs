aa8bb1caba549381aeb74631bd8da231ce4ae0d5
Sila Kuscu <silaksc27@gmail.com>
2024-10-13 21:10:37 +0300

    [Update] Updated CreateAccountDTO

--- DemoCRUDWithCleanArchitecture/Application/DTOs/Request/Account/CreateAccountDTO.cs
+++ DemoCRUDWithCleanArchitecture/Application/DTOs/Request/Account/CreateAccountDTO.cs
@@ -5,9 +5,10 @@ namespace Application.DTOs.Request.Account
     public class CreateAccountDTO : LoginDTO
     {
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         [Required, Compare(nameof(Password))]
         public string ConfirmPassword { get; set; } = string.Empty;
+        [Required]
         public string Role { get; set; } = string.Empty;
     }
 }
