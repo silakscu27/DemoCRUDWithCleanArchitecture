8c9986b635f270b5cbaae960b3c1db942c634cd0
Sila Kuscu <silaksc27@gmail.com>
2024-10-19 22:05:46 +0300

    [Add] Added IAccount interface for account and role management operations

--- DemoCRUDWithCleanArchitecture/Application/Contracts/IAccount.cs
+++ DemoCRUDWithCleanArchitecture/Application/Contracts/IAccount.cs
@@ -1,6 +1,18 @@
-﻿namespace Application.Contracts
+﻿using Application.DTOs.Request.Account;
+using Application.DTOs.Response.Account;
+using Application.DTOs.Request;
+using Application.DTOs.Response;
+
+namespace Application.Contracts
 {
     public interface IAccount
     {
+        Task CreateAdmin();
+        Task<GeneralResponse> CreateAccountAsync(CreateAccountDTO model);
+        Task<LoginResponse> LoginAccountAsync(LoginDTO model);
+        Task<GeneralResponse> CreateRoleAsync(CreateRoleDTO model);
+        Task<IEnumerable<GetRoleDTO>> GetRolesAsync();
+        Task<IEnumerable<GetUsersWithRolesResponseDTO>> GetUsersWithRolesAsync();
+        Task<GeneralResponse> ChangeUserRoleAsync(ChangeUserRoleRequestDTO model);
     }
 }
