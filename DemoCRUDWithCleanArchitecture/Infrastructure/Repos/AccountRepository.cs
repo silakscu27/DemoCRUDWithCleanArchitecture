9ec6bed8ff64c89e17cf19257244a04a1902474b
Sila Kuscu <silaksc27@gmail.com>
2024-10-20 22:16:15 +0300

    [Fix] bugs fixed

--- DemoCRUDWithCleanArchitecture/Infrastructure/Repos/AccountRepository.cs
+++ DemoCRUDWithCleanArchitecture/Infrastructure/Repos/AccountRepository.cs
@@ -3,10 +3,13 @@ using Microsoft.Extensions.Configuration;
 using Domain.Entity.Authentication;
 using Application.DTOs.Response;
 using Application.Contracts;
-using Application.DTOs.Request;
-using Application.DTOs.Response;
+using System.Text;
 using Application.DTOs.Request.Account;
 using Application.DTOs.Response.Account;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
 
 namespace Infrastructure.Repos
 {
@@ -15,6 +18,48 @@ namespace Infrastructure.Repos
         UserManager<ApplicationUser> userManager, IConfiguration config,
         SignInManager<ApplicationUser> signInManager) : IAccount
     {
+
+        private async Task<ApplicationUser> FindUserByEmailAsync(string email)
+            => await userManager.FindByEmailAsync(email);
+
+        private async Task<IdentityRole> FindRoleByNameAsync(string roleName)
+            => await roleManager.FindByNameAsync(roleName);
+
+        private static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        private async Task<string> GenerateToken(ApplicationUser user)
+        {
+            try
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                var userClaims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, user.Email),
+                        new Claim(ClaimTypes.Email, user.Email),
+                        new Claim(ClaimTypes.Role, (await userManager.GetRolesAsync(user)).FirstOrDefault() ?? ""),
+                        new Claim("Fullname", user.Name)
+                    };
+
+
+                var token = new JwtSecurityToken(
+                    issuer: config["Jwt:Issuer"],
+                    audience: config["Jwt:Audience"],
+                    claims: userClaims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: credentials
+                    );
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            catch { return null!; }
+        }
+
         Task<GeneralResponse> IAccount.ChangeUserRoleAsync(ChangeUserRoleRequestDTO model)
         {
             throw new NotImplementedException();
