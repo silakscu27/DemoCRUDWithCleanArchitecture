6355a67270bc99cf3a1da3ecf24435124ce55439
Sila Kuscu <silaksc27@gmail.com>
2024-10-20 22:12:03 +0300

    [Update] Name property added to ApplicationUser

--- DemoCRUDWithCleanArchitecture/Domain/Entity/Authentication/ApplicationUser.cs
+++ DemoCRUDWithCleanArchitecture/Domain/Entity/Authentication/ApplicationUser.cs
@@ -1,9 +1,9 @@
-﻿
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace Domain.Entity.Authentication
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
     }
 }
